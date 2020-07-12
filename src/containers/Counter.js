ab835bb73c68148041c5f18d8c19aa2bbfbd0c6e
giacomo9999 <gmarchesi888@gmail.com>
2020-07-12 11:52:46 -0400

    Clean up App, add new directories.

---
+++ src/containers/Counter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Counter = (props) => {
+  return (
+    <div className="container-inner">
+      <h2>Counter </h2>
+    </div>
+  );
+};
+
+export default Counter;
